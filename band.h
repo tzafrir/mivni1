4e1a02827a04faa1a0ba740e750db83faf276a37
Tzafrir Rehan <stzaf85@ibm-image.(none)>
2009-04-24 13:39:04 +0300

    Revert "reverted manually"
    This reverts commit 41776c88f975de65a92a910e0f23ab7d22c9caba.

--- band.h
+++ band.h
@@ -11,12 +11,10 @@ class Band {
 		: band_id(bandID), price(price), votes(0) {};
 	
 	bool operator< (Band b) {
-		return ((this->votes <= b.votes) &&
-			(this->price <= b.price) && (this->band_id < b.band_id));
+		return (this->band_id < b.band_id);
 	}
 	bool operator> (Band b) {
-		return !((this->votes <= b.votes) &&
-			(this->price <= b.price) && (this->band_id < b.band_id));
+		return (this->band_id > b.band_id);
 	}
 	friend bool operator== (Band a, Band b) {
 		return (a.band_id == b.band_id);
