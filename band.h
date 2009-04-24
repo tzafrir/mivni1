58ad220f6d8ae57a2bba3a9641af98dfaabae82d
Tzafrir Rehan <tzafrir.r@gmail.com>
2009-04-24 14:30:07 +0300

    Finalised BandByWhatever

--- band.h
+++ band.h
@@ -25,14 +25,40 @@ class BandByPrice {
 	Band* band;
 		public:
 	bool operator< (BandByPrice b) {
-		return (this->price < b.price);
+		return (this->band->price < b.band->price);
 	}
 	bool operator> (BandByPrice b) {
-		return (this->price > b.price);	
+		return (this->band->price > b.band->price);	
 	}
 	friend bool operator== (BandByPrice a, BandByPrice b) {
 		return (*a.band == *b.band);
 	}
-}
+};
+
+class BandByVotes {
+	Band* band;
+		public:
+	bool operator< (BandByVotes b) {
+		if (this->band->votes < b.band->votes) {
+			return true;
+		} else if (this->band->votes == b.band->votes) {
+			if (this->band->price < b.band->price) {
+				return true;
+			} else {
+				return (this->band->band_id < b.band->band_id);
+			}
+		}
+	}
+	bool operator> (BandByVotes b) {
+		if (*this == b) {
+			return false;
+		} else {
+			return !(*this < b);
+		}
+	}
+	friend bool operator== (BandByVotes a, BandByVotes b) {
+		return (*a.band == *b.band);
+	}
+};
 
 #endif
