7ed69dc98f4eaed434528869c85b5c3c1541c5a6
Tzafrir Rehan <tzafrir.r@gmail.com>
2009-04-26 20:21:13 +0300

    Testing infrastructure expanded to support multi testing

--- test_00.cpp
+++ test_00.cpp
@@ -3,28 +3,9 @@
 #include <string>
 #include "library1.h"
 #include <limits.h>
+#include "tests.h"
 
 using namespace std;
-
-string stat2str(StatusType stat) {
-	switch ( stat ) {
-		case ALLOCATION_ERROR	:	return "ALLOCATION_ERROR";
-		case INVALID_INPUT		:	return "INVALID_INPUT";
-		case SUCCESS			:	return "SUCCESS";
-		case FAILURE			:	return "FAILURE";
-		default					:	return "\033[31mUNDEFINED\033[0m";
-	}
-}
-
-void expect(string message, StatusType exp, StatusType result) {
-	const unsigned int len = 40;
-	message += ":";
-	while(message.length() < len) {
-		message += " ";
-	}
-	if (result == exp) cout << "\t" << message << "\033[32mOK\033[0m\t\t" + stat2str(result) << endl;
-	else cout << "\t" << message << "\033[31mFAIL\033[0m\t\t" + stat2str(result) << endl;
-}
 
 int main(int argc, char** argv) {
 	
---
+++ tests.h
@@ -0,0 +1,32 @@
+#ifndef __TESTS_H__
+#define __TESTS_H__
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string stat2str(StatusType stat) {
+	switch ( stat ) {
+		case ALLOCATION_ERROR	:	return "ALLOCATION_ERROR";
+		case INVALID_INPUT		:	return "INVALID_INPUT";
+		case SUCCESS			:	return "SUCCESS";
+		case FAILURE			:	return "FAILURE";
+		default					:	return "\033[31mUNDEFINED\033[0m";
+	}
+}
+
+void expect(string message, StatusType exp, StatusType result) {
+	const unsigned int len = 40;
+	message += ":";
+	while(message.length() < len) {
+		message += " ";
+	}
+	if (result == exp) cout << "\t" << message << "\033[32mOK\033[0m\t\t" + stat2str(result) << endl;
+	else cout << "\t" << message << "\033[31mFAIL\033[0m\t\t" + stat2str(result) << endl;
+}
+
+
+
+#endif /* __TESTS_H__ */
+
